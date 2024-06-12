242e2c2b2e636906efd41005b69478cee95520b3
Ravindra Yadav <yadavra2005@gmail.com>
2024-06-12 07:43:28 +0530

    sum function

---
+++ utils/script.js
@@ -0,0 +1,15 @@
+const hello = () => {
+    console.log('Hello, there!');
+}
+
+hello();
+
+let a = 30;
+let b = 39;
+
+function sum(a, b) {
+    return a+b;
+}
+
+let ans = sum(a, b);
+console.log(ans);
